c6b2a6ba88729221c1d6f5449dd74080189061bc
Mari <mariatimofeeva@Masenka.local>
2017-05-23 16:41:41 +0300

    Podspec

---
+++ Example/Pods/Target Support Files/MircodIOSFramework/MircodIOSFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MircodIOSFramework : NSObject
+@end
+@implementation PodsDummy_MircodIOSFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-MircodIOSFramework_Example/Pods-MircodIOSFramework_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MircodIOSFramework_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MircodIOSFramework_Example
+@end
